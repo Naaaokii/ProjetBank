4aed6533246125bdbbb63d92419d7be8ebdacd55
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-17 11:58:22 +0100

    index

--- src/templates/partials/footer.php
+++ src/templates/partials/footer.php
@@ -1,3 +1,19 @@
-<footer>
-    <p>&copy; <?= date('Y'); ?></p>
-</footer>
+<div>
+    <h2 class="space">Information</h2>
+    <p class="color_gray">On veut toute votre moula !!!</p>
+    <p class="color_orange">Tel : <span style="color:white"> 06 05 04 03 02</span></p>
+    <p class="color_orange">Email : <span style="color:white"> support@bankade.com</span></p>
+    <p class="color_orange">Location : <span style="color:white"> Paris</span></p>
+    <div>
+        <i class="foo-brands fa-brands fa-facebook-f"></i> <i class="foo-brands fa-brands fa-twitter"></i> <i class="foo-brands fa-brands fa-google"></i> <i class="foo-brands fa-brands fa-pinterest"></i> <i class="foo-brands fa-brands fa-instagram"></i>
+    </div>
+    <p class="color_gray">Copyright © 2023 Tous droits réservés</p>
+</div>
+<div class="footerdiv">
+    <h2>Bank ADE</h2>
+    <ul class="move_lines">
+        <li class="color_orange"><span style="color: rgb(168, 167, 167)"><a href="#" class="footlink">Virements</a></span></li>
+        <li class="color_orange"><span style="color: rgb(168, 167, 167)"><a href="#" class="footlink">Depots</a></span></li>
+        <li class="color_orange"><span style="color: rgb(168, 167, 167)"><a href="#" class="footlink">Convertisseur</a></span></li>
+    </ul>
+</div>
---
+++ src/templates/partials/header.php
@@ -0,0 +1,12 @@
+<section>
+    <nav class="navbar">
+        <h3>BankADE</h3>
+        <div class="divnav">
+            <a href="index.php" class="correctlink">ACCUEIL</a>
+            <a href="#" class="correctlink">VIREMENTS</a>
+            <a href="#" class="correctlink">DEPOTS</a>
+            <a href="#" class="correctlink">CONVERTISSEUR</a>
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </div>
+    </nav>
+</section>
