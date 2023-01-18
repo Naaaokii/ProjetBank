d56abe59a6dd16b21fafe46fdd66500af8cd405f
SiERRA74 <aksierra74@gmail.com>
2023-01-18 09:04:20 +0100

    tkt frero

--- src/class/ContactForm.php
+++ src/class/ContactForm.php
@@ -1,6 +1,6 @@
 <?php
 
-class ContactForm extends DbObject {
+Class ContactForm extends DbObject {
     public $id;
 	public $fullname;
 	public $phone;
@@ -10,4 +10,23 @@ class ContactForm extends DbObject {
 
 }
 
+Class UserForm extends DbObject {
+	 public $id;
+	 public $nom; 
+	 public $prenom;
+	 public $email;
+	 public $telephone;
+	 public $date_de_naissance;
+	 public $motdepasse;
+	 public $role;
+
+}
+
+Class CompteForm extends DbObject {
+	public $id_cmpt;
+	public $numero; 
+	public $id_user;
+	public $id_monaie;
+	public $solde;
+}
 ?>
