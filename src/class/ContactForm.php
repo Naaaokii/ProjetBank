b63ff2129df271b5e61e7b4ab940dd80a0f2ba00
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-18 16:17:16 +0100

    creation comptes

--- src/class/ContactForm.php
+++ src/class/ContactForm.php
@@ -7,7 +7,26 @@ class ContactForm extends DbObject {
 	public $email;
 	public $message;
 	public $created_at;
+}
 
+Class User extends DbObject {
+	public $id;
+	public $nom; 
+	public $prenom;
+	public $email;
+	public $telephone;
+	public $date_de_naissance;
+	public $motdepasse;
+	public $role;
+
+}
+
+Class Compte extends DbObject {
+   public $id_cmpt;
+   public $numero; 
+   public $id_user;
+   public $id_monaie;
+   public $solde;
 }
 
 ?>
--- www/index.php
+++ www/index.php
@@ -5,13 +5,26 @@ require_once __DIR__ . '/../src/init.php';
 // $_SESSION
 
 if(isset($_POST['creer'])){
-    if(ISSET($_POST['monnaie'], $_POST['solde']) && !empty($_POST['monnaie']) && !empty($_POST['solde'])){
+    if(isset($_POST['monnaie'], $_POST['solde']) && !empty($_POST['monnaie']) && !empty($_POST['solde'])){
+        $mail = $_SESSION['email'];
+        $req = $dbh->prepare('SELECT id FROM users WHERE email = :email');
+        $req->execute(array('email' => $mail));
+        $user = $req->fetch();
+        $idUser = $user['id'];
+         
+        $sth = $dbh->prepare('SELECT id_Monaie FROM monaies WHERE nom = :nom');
+        $sth->execute(array('nom' => $_POST['monnaie']));
+        $idMonaie = $sth->fetch();
+
+        foreach($idMonaie as $value){
+            $monnaie = $value;
+        }
+
         $numero = rand(10000000000,99999999999);
-        $monnaie = $_POST['monnaie'];
         $solde = $_POST['solde'];
 
-        $sth = $dbh->prepare("INSERT INTO comptes (numero, id_user, id_monnaie, solde) VALUES (?,?,?,?)");
-        $sth->execute([$name, $firstName, $email, $number, $date, hash('sha256', $password), 'unverified']);
+        $sth = $dbh->prepare("INSERT INTO comptes (numero, id_user, id_monaie, solde) VALUES (?,?,?,?)");
+        $sth->execute([$numero, $idUser, $monnaie, $solde]);
     }
 }
 
@@ -35,7 +48,7 @@ if(isset($_POST['creer'])){
                     <option value="" class="sous_theme4">Chosir la monnaie</option>
                     <option value="euro">Euro</option>
                     <option value="usd">USD</option>
-                    <option value="pessos">Pessos</option>
+                    <option value="pesos">Pesos</option>
                     <option value="yen">Yen</option>
                     <option value="ls">LS</option>
                 </select>
