cbe24ff81df49db6f667fa1f8c920080594e8434
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-19 00:14:59 +0100

    retraits

--- src/templates/partials/header.php
+++ src/templates/partials/header.php
@@ -5,6 +5,7 @@
             <a href="index.php" class="correctlink">ACCUEIL</a>
             <a href="virements.php" class="correctlink">VIREMENTS</a>
             <a href="depots.php" class="correctlink">DEPOTS</a>
+            <a href="retraits.php" class="correctlink">RETRAITS</a>
             <a href="convertisseur.php" class="correctlink">CONVERTISSEUR</a>
             <a href="login.php"><i class="fa-solid fa-user"></i></a>
         </div>
---
+++ www/retraits.php
@@ -0,0 +1,80 @@
+<?php
+
+require_once __DIR__ . '/../src/init.php';
+// $db
+// $_SESSION
+
+if(isset($_POST['retrait'])){
+    if(isset($_POST['numberaccount'], $_POST['solde']) && !empty($_POST['numberaccount']) && !empty($_POST['solde'])){
+        
+        $numberAccount = $_POST['numberaccount'];
+        $soldeDepot = $_POST['solde'];
+
+        // Récupérer l'id de l'utilisateur connecté
+        $mail = $_SESSION['email'];
+        $req = $dbh->prepare('SELECT id FROM users WHERE email = :email');
+        $req->execute(array('email' => $mail));
+        $user = $req->fetch();
+        $idUser = $user['id'];
+
+        // Récupérer l'id_user par rapport au compte
+        $sth = $dbh->prepare('SELECT id_user FROM comptes WHERE numero = :numero0');
+        $sth->execute(array('numero0' => $numberAccount));
+        $user = $sth->fetch();
+        $userId = $user['id_user'];
+
+        // Si le numero de compte de l'expediteur est bien celui de la personne connecté
+        if ($idUser == $userId){
+
+            $req = $dbh->prepare('SELECT solde, id_monaie FROM comptes WHERE numero = :numero');
+            $req->execute(array('numero' => $numberAccount));
+            $monaieId = $req->fetch();
+            $soldeActuelle = $monaieId['solde'];
+            $idMonaie = $monaieId['id_monaie'];
+
+            $sth = $dbh->prepare('SELECT nom FROM monaies WHERE id_monaie = :id_monaie');
+            $sth->execute(array('id_monaie' => $idMonaie));
+            $monaie = $sth->fetch();
+            $nomMonaie = $monaie['nom'];
+
+            if ($soldeActuelle > $soldeDepot){
+                $soldeTotal = $soldeActuelle - $soldeDepot;
+            
+                $sth = $dbh->prepare("UPDATE comptes SET solde = :solde WHERE numero = ".$numberAccount);
+                $sth->execute(['solde' => $soldeTotal]);
+
+                echo "Vous avez bien retiré ".$soldeDepot." ".$nomMonaie.", il vous reste ".$soldeTotal." ".$nomMonaie;
+            }
+        }else{
+            echo 'Numéro de compte invalide';
+        }
+    }
+}
+?>
+
+<?php
+    $page_title = 'RETRAITS';
+    require_once __DIR__ . '/../src/templates/partials/html_head.php';
+?>
+    <body>
+        <header>
+            <?php
+            require_once __DIR__ . '/../src/templates/partials/header.php';
+            ?>
+        </header>
+        <h1 class='title'>RETRAITS</h1>
+        <div class="createaccount">
+            <h2 class='titlecreateaccount'>Retirer de l'argent</h2>
+            <form method="post">
+                <input class="numberaccount" type="number"  name="numberaccount"  id="numberaccount"  placeholder="Numéro du compte" required>
+                <input class="solde" type="number"  name="solde"  id="solde"  placeholder="Solde" required>
+                <input class='buttondaccount' type="submit" value="Retirer" name="retrait" class="inpbutton">
+            </form>
+        </div>
+        <footer class='footer'>
+            <?php 
+                require_once __DIR__ . '/../src/templates/partials/footer.php'; 
+            ?>
+        </footer>  
+    </body>
+</html>
