d95ab58ad6bde240ac933df474f804dade6cdb34
Naaaokii <117320105+Naaaokii@users.noreply.github.com>
2023-01-18 12:17:09 +0100

    comptes

--- www/index.php
+++ www/index.php
@@ -4,6 +4,17 @@ require_once __DIR__ . '/../src/init.php';
 // $db
 // $_SESSION
 
+if(isset($_POST['creer'])){
+    if(ISSET($_POST['monnaie'], $_POST['solde']) && !empty($_POST['monnaie']) && !empty($_POST['solde'])){
+        $numero = rand(10000000000,99999999999);
+        $monnaie = $_POST['monnaie'];
+        $solde = $_POST['solde'];
+
+        $sth = $dbh->prepare("INSERT INTO comptes (numero, id_user, id_monnaie, solde) VALUES (?,?,?,?)");
+        $sth->execute([$name, $firstName, $email, $number, $date, hash('sha256', $password), 'unverified']);
+    }
+}
+
 ?>
 
 <?php
@@ -16,7 +27,22 @@ require_once __DIR__ . '/../src/init.php';
             require_once __DIR__ . '/../src/templates/partials/header.php';
             ?>
         </header>
-        <h1 class='title'>Accueil</h1>
+        <h1 class='title'>Mes comptes</h1>
+        <div class="createaccount">
+            <h2 class='titlecreateaccount'>Créer un compte</h2>
+            <form method="post">
+                <select name="monnaie" id="monnaie">
+                    <option value="" class="sous_theme4">Chosir la monnaie</option>
+                    <option value="euro">Euro</option>
+                    <option value="usd">USD</option>
+                    <option value="pessos">Pessos</option>
+                    <option value="yen">Yen</option>
+                    <option value="ls">LS</option>
+                </select>
+                <input class="solde" type="number"  name="solde"  id="solde"  placeholder="Solde" required>
+                <input class='buttondaccount' type="submit" value="Créér" name="creer" class="inpbutton">
+            </form>
+        </div>
         <footer class='footer'>
             <?php 
                 require_once __DIR__ . '/../src/templates/partials/footer.php'; 
