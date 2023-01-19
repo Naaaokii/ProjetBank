e216a20b2655ee17e8067e95e4e220849a190b54
SiERRA74 <aksierra74@gmail.com>
2023-01-19 10:45:38 +0100

    Squashed commit of the following:
    commit f7bb358c8f1647dfb2ef622132add8489ab4e521
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Thu Jan 19 01:28:53 2023 +0100
    
        convertisseur
    
    commit cbe24ff81df49db6f667fa1f8c920080594e8434
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Thu Jan 19 00:14:59 2023 +0100
    
        retraits
    
    commit 790516754d1292d053c42f7f2fe04b872b7e5595
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Wed Jan 18 23:46:11 2023 +0100
    
        verif id et id_user pour depot
    
    commit 7f7bd896da7201410f5dd453af7ad6a414d1752a
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Wed Jan 18 23:36:52 2023 +0100
    
        gérer id_user virements
    
    commit 7ea76b2df878416314fb77087395a0cde1febc70
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Wed Jan 18 23:15:01 2023 +0100
    
        virements
    
    commit 9ed27870f6e1c14f0cd883f660fffda999472015
    Author: Naaaokii <117320105+Naaaokii@users.noreply.github.com>
    Date:   Wed Jan 18 22:26:14 2023 +0100
    
        depots comptes

--- www/retraits.php
+++ www/retraits.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../src/init.php';
 // $db
 // $_SESSION
 
-if(isset($_POST['depot'])){
+if(isset($_POST['retrait'])){
     if(isset($_POST['numberaccount'], $_POST['solde']) && !empty($_POST['numberaccount']) && !empty($_POST['solde'])){
         
         $numberAccount = $_POST['numberaccount'];
@@ -26,15 +26,27 @@ if(isset($_POST['depot'])){
         // Si le numero de compte de l'expediteur est bien celui de la personne connecté
         if ($idUser == $userId){
 
-            $req = $dbh->prepare('SELECT solde FROM comptes WHERE numero = :numero');
+            $req = $dbh->prepare('SELECT solde, id_monaie FROM comptes WHERE numero = :numero');
             $req->execute(array('numero' => $numberAccount));
-            $soldeAccount = $req->fetch();
-            $soldeActuelle = $soldeAccount['solde'];
+            $monaieId = $req->fetch();
+            $soldeActuelle = $monaieId['solde'];
+            $idMonaie = $monaieId['id_monaie'];
 
-            $soldeTotal = $soldeActuelle + $soldeDepot;
+            $sth = $dbh->prepare('SELECT nom FROM monaies WHERE id_monaie = :id_monaie');
+            $sth->execute(array('id_monaie' => $idMonaie));
+            $monaie = $sth->fetch();
+            $nomMonaie = $monaie['nom'];
+
+            if ($soldeActuelle > $soldeDepot){
+                $soldeTotal = $soldeActuelle - $soldeDepot;
             
-            $sth = $dbh->prepare("UPDATE comptes SET solde = :solde WHERE numero = ".$numberAccount);
-            $sth->execute(['solde' => $soldeTotal]);
+                $sth = $dbh->prepare("UPDATE comptes SET solde = :solde WHERE numero = ".$numberAccount);
+                $sth->execute(['solde' => $soldeTotal]);
+
+                echo "Vous avez bien retiré ".$soldeDepot." ".$nomMonaie.", il vous reste ".$soldeTotal." ".$nomMonaie;
+            }else{
+                echo "T'as pas assez de thunes loser";
+            }  
         }else{
             echo 'Numéro de compte invalide';
         }
@@ -43,7 +55,7 @@ if(isset($_POST['depot'])){
 ?>
 
 <?php
-    $page_title = 'DEPOTS';
+    $page_title = 'RETRAITS';
     require_once __DIR__ . '/../src/templates/partials/html_head.php';
 ?>
     <body>
@@ -52,13 +64,13 @@ if(isset($_POST['depot'])){
             require_once __DIR__ . '/../src/templates/partials/header.php';
             ?>
         </header>
-        <h1 class='title'>DEPOTS</h1>
+        <h1 class='title'>RETRAITS</h1>
         <div class="createaccount">
-            <h2 class='titlecreateaccount'>Faire un dépot</h2>
+            <h2 class='titlecreateaccount'>Retirer de l'argent</h2>
             <form method="post">
                 <input class="numberaccount" type="number"  name="numberaccount"  id="numberaccount"  placeholder="Numéro du compte" required>
                 <input class="solde" type="number"  name="solde"  id="solde"  placeholder="Solde" required>
-                <input class='buttondaccount' type="submit" value="Déposer" name="depot" class="inpbutton">
+                <input class='buttondaccount' type="submit" value="Retirer" name="retrait" class="inpbutton">
             </form>
         </div>
         <footer class='footer'>
